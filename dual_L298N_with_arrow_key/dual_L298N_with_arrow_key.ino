6784b7b75343f07ca5c1634ddab1ad48dae80fb7
Prashant Lalwani <plalwani.purdue@gmail.com>
2017-10-29 18:49:45 -0700

    completed and tested the code. Serial input => 8: All motors forward, 4: left motors forward, 6: Right motors forward, 2: All motors backward, 5: STOP

--- dual_L298N_with_arrow_key/dual_L298N_with_arrow_key.ino
+++ dual_L298N_with_arrow_key/dual_L298N_with_arrow_key.ino
@@ -1,18 +1,41 @@
 //Test code with dual L298N motor drivers (4 motors)that utilizes serial input from keyboard
-// to turn on all four motors in specific direction to move a rover in different directions
+// to turn all four motors in specific direction to move a rover in different directions
+
+//8: All motors forward
+//2: All motors backwards
+//4: Left motors forward
+//6: Right motors forward
+//5: STOP
+
 // Author: Prashant Lalwani
 // 10/27/2017
 
 
-// defines for ASCII input
+
+//////////////////////////////
+// defines for ASCII input //
+////////////////////////////
+
 #define LEFT 52
 #define RIGHT 54
 #define UP 56
 #define DOWN 50
+#define STOP 53
 
-//defines to send to motor driver function indicating direction of motors
+
+
+/////////////////////////////////////////////////////////////////////////////
+//defines to send to motor driver function indicating direction of motors //
+///////////////////////////////////////////////////////////////////////////
+
 #define FORWARD 8
 #define BACKWARD 2
+
+
+
+///////////////////////
+// global variables //
+/////////////////////
 
 int direction;
 
@@ -26,50 +49,137 @@ int left_motor_driver[2][3] =
 // right motor driver array specifying pinouts 
 int right_motor_driver[2][3] =
 {
-  {8,7,6}, //IN1,IN2,ENA
-  {11,10,9}, //IN3,IN4,ENB
+  {7,8,6}, //IN1,IN2,ENA
+  {10,11,9}, //IN3,IN4,ENB
 };
+
+
+
+////////////
+// Setup //
+//////////
 
 void setup() 
 {
-  Serial.begin(9600);
+  Serial.begin(9600); // initialize Serial
+  
+  // setup all pins being used by both motor driver as OUTPUT
+  for(int i=0; i<2; i++) // loop over both motors
+  {
+    for(int j=0; j<3; j++) // loop over all pins of motor
+    {
+      pinMode(left_motor_driver[i][j],OUTPUT);
+      pinMode(right_motor_driver[i][j],OUTPUT);  
+    }
+  }
+  Serial.println("8: All motors forward\n2: All motors backwards\n4:left motors forward\n6: right motors forward\n5: STOP");
 }
+
+
+
+///////////
+// Main //
+/////////
 
 void loop() 
 {
+  // Assign direction received from Serial port
   if(Serial.available() > 0){    
     direction = Serial.read();
   }
+
+  // drive motors as per Serial input
   switch (direction){
-    
+      
     case UP:
-    Serial.println("received UP\nDriving all motors forward");
+    drive_left_motors(FORWARD);
+    drive_right_motors(FORWARD);
     break;
 
     case DOWN:
     //down
-    Serial.println("received DOWN\nDriving all motors backwards");
+    drive_left_motors(BACKWARD);
+    drive_right_motors(BACKWARD);
     break;
 
     case LEFT:
     // left
-    Serial.println("received UP\nDriving left motors forward");
+    drive_left_motors(FORWARD);
+    drive_right_motors(STOP);
     break;
 
     case RIGHT:
     //right
-    Serial.println("received UP\nDriving right motors forward");
+    drive_left_motors(STOP);
+    drive_right_motors(FORWARD);
     break;
+
+    case STOP:
+    //stop
+    drive_left_motors(STOP);
+    drive_right_motors(STOP);
   }
 }
 
+
+
+////////////////////////////////////
+// function to drive left motors //
+//////////////////////////////////
+
 void drive_left_motors(int motor_direction)
-{
-  
+{  
+  for(int i = 0; i < 2; i++)
+  {
+    switch(motor_direction)
+    {
+      case FORWARD:
+      digitalWrite(left_motor_driver[i][0], HIGH); //IN1: high
+      digitalWrite(left_motor_driver[i][1], LOW); //IN2: low
+      analogWrite(left_motor_driver[i][2],128); //EN: PWM
+      break;
+
+      case BACKWARD:
+      digitalWrite(left_motor_driver[i][0], LOW); //IN1: low
+      digitalWrite(left_motor_driver[i][1], HIGH); //IN2: high
+      analogWrite(left_motor_driver[i][2],128); //EN: PWM 
+      break;
+
+      case STOP:
+      analogWrite(left_motor_driver[i][2],0); //EN: low
+      break;
+    }
+  }
 }
+
+
+
+/////////////////////////////////////
+// function to drive right motors //
+///////////////////////////////////
 
 void drive_right_motors(int motor_direction)
 {
-  
+   for(int i = 0; i < 2; i++)
+  {
+    switch(motor_direction)
+    {
+      case FORWARD:
+      digitalWrite(right_motor_driver[i][0], HIGH); //IN1: high
+      digitalWrite(right_motor_driver[i][1], LOW); //IN2: low
+      analogWrite(right_motor_driver[i][2],128); //EN: PWM
+      break;
+
+      case BACKWARD:
+      digitalWrite(right_motor_driver[i][0], LOW); //IN1: low
+      digitalWrite(right_motor_driver[i][1], HIGH); //IN2: high
+      analogWrite(right_motor_driver[i][2],128); //EN: PWM
+      break;
+
+      case STOP:
+      analogWrite(right_motor_driver[i][2],0); //EN: low
+      break;
+    }
+  }
 }
 
